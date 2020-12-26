027e882d36e4c205f52fe8aa16d63c53f3fb1338
worione <hyorax@gmail.com>
2020-12-26 21:05:39 +0300

    Add Controllers  ⚡

--- routes/mainRouter.js
+++ routes/mainRouter.js
@@ -5,14 +5,21 @@ const got = require('got')
 const addData = require('../database/addData')
 const allData = require('../database/allData')
 const singleData = require('../database/singleData')
-const issueRouter = require('./issueRouter')
+
+const {
+    getAllDataToDB,
+    addDataToDB,
+    getSingleDataToDB
+} = require('../controllers/database');
+
+const { getIssues } = require('../controllers/issue')
 const notFound = require('./notFound')
 
 // routers middleware
 router.get('/', (req, res) => res.send('Hello World!'))
 
 // issues middleware
-router.get('/:user/:repo', issueRouter)
+router.get('/:user/:repo', getIssues)
 
 // Add - Get Mongo
 router.get('/add', addData)
