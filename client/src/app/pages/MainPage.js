e4ec6db286db6776d07dbf2f237d04c8fe95180c
worione <hyorax@gmail.com>
2020-12-31 09:25:55 +0300

    Small Add

--- client/src/app/pages/MainPage.js
+++ client/src/app/pages/MainPage.js
@@ -2,15 +2,18 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 const MainPage = () => {
     const [getData, setGetData] = useState([]);
-    useEffect(() => {
-        axios.get('https://api.github.com/repos/furkanportakal/opensourceadam/issues', getData)
+    const dataFetch = () => {
+        axios.get('https://api.github.com/repos/furkanportakal/opensourceadam/issues')
             .then((res) => { console.log(res) })
+    }
+    useEffect(() => {
+        dataFetch();
     });
     return (
         <div>
             <button onClick={() => getData}>Add</button>
             {
-                console.log(getData)
+                console.log(getData.url)
             }
         </div>
     );
