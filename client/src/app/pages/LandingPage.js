55a001146b8ad1a64caf2854cfc1ee9e0d099a4d
OzlemTS <ozlemturkerii@gmail.com>
2021-01-08 16:55:58 +0300

    navbar is added

--- client/src/app/pages/LandingPage.js
+++ client/src/app/pages/LandingPage.js
@@ -1,12 +1,11 @@
 import React from "react";
 import { Link } from 'react-router-dom';
+import { NavBar } from '../components/nav-bar';
+
 const LandingPage = () => {
   return (
-    <div className="min-h-screen flex flex-col items-center bg-gray-800 text-yellow-500 hover:text-white-500">
-          <h1>Open Source Adam</h1>
-          <Link to="/projects">Projects</Link>
-          <Link to="/start">Start</Link>
-          <Link to="/doc">Documents</Link>
+    <div className="min-h-screen flex flex-col items-center bg-gray-800">
+          <NavBar></NavBar>
     </div>
   );
 };
--- client/src/index.js
+++ client/src/index.js
@@ -14,4 +14,5 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// On page load or when changing themes, best to add inline in `head` to avoid FOUC
 reportWebVitals();
