f87c58446cca1560b88ce66133e0d0ae0718c9dc
Altay Simsek <altaysimsek16@gmail.com>
2021-01-20 23:12:01 +0300

    Update on controller and some fixes

--- routes/mainRouter.js
+++ routes/mainRouter.js
@@ -2,14 +2,13 @@ const express = require("express");
 const router = express.Router();
 
 const {
-    getAllRepos,
-    addDataToDB,
-    getSingleDataToDB,
-    addRepo,
+  getAllRepos,
+  addRepo,
+  getSpecificRepo,
 } = require("../controllers/database");
 
 const { githubCallback } = require("../helpers/githubCallback.js");
-const { getIssues } = require("../controllers/issue");
+
 const notFound = require("./notFound");
 
 // github callback
@@ -21,12 +20,11 @@ router.get("/", (req, res) => res.send("Hello World!"));
 // issues middleware
 //router.get("/:user/:repo", getIssues);
 
-// issue add
-router.post("/start/repos", addRepo);
-router.get("/start/repos", getAllRepos);
-
-// Add - Get Mongo
-router.get("/add", addDataToDB);
-router.get("/single", getSingleDataToDB);
+// Add new repo
+router.post("/repos", addRepo);
+// Get repos
+router.get("/repos", getAllRepos);
+// Get specific repo
+router.get("/repos/:reponame", getSpecificRepo);
 
 module.exports = router;
